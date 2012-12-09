d5403eced799dac975cfe0cf5f16bea9f437acd3
Lech Stawikowski <lech.stawikowski@gmail.com>
2012-12-09 11:46:46 +0100

    wprowadzone puste funkcje do 'DrawTouch'

--- js/DrawTouch.class.js
+++ js/DrawTouch.class.js
@@ -1,5 +1,37 @@
 DrawTouch = function(){
 
+    this.parameters = {radius: 20, width: 5, pastAlpha: 0.5, presentAlpha: 0.8,
+        startColor: 'hsl(120,100%,50%)', moveColor: 'hsl(60,100%,50%)', endColor: 'hsl(0,100%,50%)'
+    };
 
+    this.events = [];
+
+    this.setEvents = function(type,touch,target,changed){
+
+    };
+
+    this.drawTouch = function(type,ctx){
+
+    };
+
+    this.drawTarget = function(type,ctx){
+
+    };
+
+    this.drawChanged = function(type,ctx){
+
+    };
+
+    this.drawPast = function(type,ctx,changed){
+
+    };
+
+    this.drawPresent = function(type,ctx,touch,target,changed){
+
+    };
+
+    this.clear = function(ctx){
+
+    };
 
 };
